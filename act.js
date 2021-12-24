a04f61a8413d8c2a04580d4ae2dee630d4a4517d
shealfild <50893939+shealfild@users.noreply.github.com>
2021-12-24 15:39:51 +0900

    Add files via upload

--- act.js
+++ act.js
@@ -1,9 +1,9 @@
 
-v_land.init();
+//v_land.init();
 
 //v_zm.init();
 //var cm = v_men.init();
 //v_lock.init();
 // v_wheel.init();
-// v_plant.init();
-v_house.init();
+//v_plant.init();
+//v_house.init();
